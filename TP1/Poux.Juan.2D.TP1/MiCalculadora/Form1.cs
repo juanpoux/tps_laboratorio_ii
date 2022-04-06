e479a60887f6b9cb55f97fa0094274903454ed7c
Juan Poux <poux_juan@hotmail.com>
2022-04-06 01:50:54 -0300

    asd

--- TP1/Poux.Juan.2D.TP1/MiCalculadora/Form1.cs
+++ TP1/Poux.Juan.2D.TP1/MiCalculadora/Form1.cs
@@ -17,5 +17,27 @@ namespace MiCalculadora
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string texto1 = txtNumero1.Text;
+            string texto2 = txtNumero2.Text;
+
+            txtResultado.Text = (int.Parse(texto1) + int.Parse(texto2)).ToString();
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
